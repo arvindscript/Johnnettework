82f33fb04b4102dc87b65531a1fb917638b83198
arvindscript <arvindsonkar685@gmail.com>
2025-02-05 16:41:03 +0530

    apppasswordkey

--- Portal_Project/Portal_Project/settings.py
+++ Portal_Project/Portal_Project/settings.py
@@ -132,5 +132,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'arvindsonkar685@gmail.com'
-EMAIL_HOST_PASSWORD = 'skczxhqcuomqhykt'
+EMAIL_HOST_PASSWORD = 'psnmrantwhkhexas'
 EMAIL_USE_SSL = False
